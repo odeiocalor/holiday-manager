00e930c1903c7eb9df9d33f2d38f97ec2088a66c
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-14 17:50:47 -0300

    .env type safety implementation

---
+++ src/env.js
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+    DATABASE_URL: z.string().url(),
+    AUTH_SECRET: z.string(),
+    AUTH_GOOGLE_ID: z.string(),
+    AUTH_GOOGLE_SECRET: z.string(),
+});
+
+export const env = envSchema.parse(process.env);
