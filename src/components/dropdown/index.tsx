c9022ea030670d3492004f0e2b7783a48fe72bf6
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-17 18:59:06 -0300

    Dropdown component adjustment
    Adjustment on element and dropdown spacing when positionY=top

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -10,12 +10,11 @@ interface DropdownProps {
 export default function Dropdown({ positionX, positionY, children, className }: DropdownProps) {
     return (
         <div className={cn(
-            "absolute mt-1 p-2 rounded-md shadow-lg",
+            "absolute p-2 rounded-md shadow-lg",
             "bg-neutral-200 dark:bg-neutral-700",
             "hidden group-hover:block",
-            "before:absolute before:left-0 before:-top-4 before:h-4 before:w-full",
-            { "top-full": positionY === "top" },
-            { "bottom-full": positionY === "bottom" },
+            { "top-full mt-1 before:absolute before:left-0 before:-top-4 before:h-4 before:w-full": positionY === "bottom" },
+            { "bottom-full mb-1 after:absolute after:left-0 after:-bottom-4 after:h-4 after:w-full": positionY === "top" },
             { "left-0": positionX === "left" },
             { "right-0": positionX === "right" },
             className
