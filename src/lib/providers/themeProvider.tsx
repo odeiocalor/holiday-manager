742d5387432db72724a23383231b6006e6b34868
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-18 02:19:21 -0300

    ThemeProvider adjustment
    CSS transitions disabled when changing themes

--- src/lib/providers/themeProvider.tsx
+++ src/lib/providers/themeProvider.tsx
@@ -4,5 +4,5 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import type { ThemeProviderProps } from "next-themes/dist/types";
 
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    return <NextThemesProvider {...props} attribute="class" defaultTheme='system' enableSystem>{children}</NextThemesProvider>
+    return <NextThemesProvider {...props} attribute="class" defaultTheme='system' enableSystem disableTransitionOnChange>{children}</NextThemesProvider>
 }
