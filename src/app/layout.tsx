e98c761f00e69b0e6d8ad24f85bf97c0034f7360
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-18 22:24:27 -0300

    RootLayout adjustment
    .Padding top added on mobile viewport to offset the DashboardMobileHeader height as it is fixed on the top of the screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
     return (
         <html lang="pt-BR" suppressHydrationWarning>
             <body className={cn(
-                "min-h-screen min-w-full flex font-sans antialiased pb-14 md:pb-0",
-                "bg-neutral-50 dark:bg-neutral-800",
+                "min-h-screen min-w-full flex font-sans antialiased pb-14 pt-[4.5rem] md:pb-0 md:pt-0",
+                "bg-neutral-200 dark:bg-neutral-800",
                 "text-neutral-900 dark:text-neutral-100",
                 fontSans.variable
             )}>
