4192afa113da6018e49fdf64c236dcd8a58f82e8
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-18 22:27:05 -0300

    DashboardMobileHeader adjusments
    .Fixed to the top of the page
    .bg-color added
    .h1 tag added to the Logo component

--- src/components/header/dashboardMobileHeader.tsx
+++ src/components/header/dashboardMobileHeader.tsx
@@ -9,13 +9,17 @@ import { cn } from "@/lib/utils"
 export default async function DashboardMobileHeader() {
     return (
         <MaxWidthWrapper className={cn(
-            "block md:hidden",
+            "block md:hidden fixed top-0 left-0 right-0 z-10",
+            "bg-neutral-100 dark:bg-neutral-900",
         )}>
             <header className={cn(
                 "flex items-center justify-between"
             )}>
-                <Link href="/" className="text-3xl font-bold">
-                    <Logo />
+                <Link href="/">
+                    <h1>
+                        <span className="sr-only">Holiday Manager</span>
+                        <Logo />
+                    </h1>
                 </Link>
                 <UserIcon />
             </header>
