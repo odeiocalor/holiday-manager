7e060c84559edabcbff10803a23827443fc7891c
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-20 03:31:11 -0300

    .env update

--- src/env.js
+++ src/env.js
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
+    APP_URL: z.string().url(),
+    APP_ENV: z.enum(['development', 'test', 'production']),
     DATABASE_URL: z.string().url(),
     AUTH_SECRET: z.string(),
     AUTH_GOOGLE_ID: z.string(),
