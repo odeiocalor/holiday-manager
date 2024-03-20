276be9356e952a19ca547edc4ecd1fc0f57900e8
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-20 03:32:55 -0300

    Dropdown component cva implementation

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,23 +1,33 @@
+import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
-interface DropdownProps {
-    positionX?: "left" | "right"
-    positionY?: "top" | "bottom"
-    children: React.ReactNode
-    className?: string
-}
+const dropdownVariants = cva(
+    "absolute p-4 rounded-md shadow-lg bg-neutral-300 dark:bg-neutral-700 hidden group-hover:block",
+    {
+        variants: {
+            positionX: {
+                left: "left-0",
+                right: "right-0",
+            },
+            positionY: {
+                bottom: "top-full mt-1 before:absolute before:left-0 before:-top-4 before:h-4 before:w-full",
+                top: "bottom-full mb-1 after:absolute after:left-0 after:-bottom-4 after:h-4 after:w-full",
+            },
+        },
+        defaultVariants: {
+            positionX: "right",
+            positionY: "bottom",
+        },
+    }
+
+)
+
+interface DropdownProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof dropdownVariants> { }
 
 export default function Dropdown({ positionX, positionY, children, className }: DropdownProps) {
     return (
         <div className={cn(
-            "absolute p-4 rounded-md shadow-lg",
-            "bg-neutral-300 dark:bg-neutral-700",
-            "hidden group-hover:block",
-            { "top-full mt-1 before:absolute before:left-0 before:-top-4 before:h-4 before:w-full": positionY === "bottom" },
-            { "bottom-full mb-1 after:absolute after:left-0 after:-bottom-4 after:h-4 after:w-full": positionY === "top" },
-            { "left-0": positionX === "left" },
-            { "right-0": positionX === "right" },
-            className
+            dropdownVariants({ positionX, positionY, className }),
         )}>
             {children}
         </div>
