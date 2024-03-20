1185a547f61cbaa42278d762304a2ad390bb5fbc
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-20 03:31:26 -0300

    Captilize utility function

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -10,3 +10,8 @@ export const cn = (...inputs: ClassValue[]) => {
 export const navItemIsActive = ({ pathname, href }: { pathname: string, href: string }) => {
     return pathname === href || href !== "/" ? pathname?.startsWith(href) : false
 }
+
+export const capitalize = (s: string) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
