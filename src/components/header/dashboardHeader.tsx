c95fd2d14d999b094066e28848238361eb345879
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-03-20 03:40:08 -0300

    Breadcrumbs in place of HeaderTitle on DashboardHeader

--- src/components/header/dashboardHeader.tsx
+++ src/components/header/dashboardHeader.tsx
@@ -1,19 +1,23 @@
 import { Container } from "@/components/structure"
 import { UserIcon } from "@/components/user/userIcon"
-import HeaderTitle from "@/components/header/headerTitle"
+import Breadcrumbs from "@/components/nav/breadcrumbs"
 
 import { cn } from "@/lib/utils"
 
 export default function DashboardHeader() {
     return (
-        <Container className={cn(
-            "hidden md:block bg-neutral-100 dark:bg-neutral-900",
+        <Container padding="p-xl" className={cn(
+            "lg:py-4",
+            "hidden md:block",
+            "bg-neutral-100 dark:bg-neutral-900",
         )}>
             <header className={cn(
                 "flex items-center justify-between"
             )}>
-                <HeaderTitle />
-                <UserIcon />
+                <Breadcrumbs />
+                <div className={cn("ml-auto")}>
+                    <UserIcon />
+                </div>
             </header>
         </Container>
     )
