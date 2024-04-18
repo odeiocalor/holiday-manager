a8e4807fddcaa35d496e084981f654dd658873ec
Carlos Souza <carlosalbertojuvino@gmail.com>
2024-04-17 22:34:40 -0300

    Button component variants adjustment
    .New attribute "width"
    .Disabled styles for all variants

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -11,18 +11,24 @@ const buttonVariants = cva(
                 md: "p-3",
                 lg: "p-4",
             },
+            width: {
+                full: "w-full",
+                auto: "w-auto",
+                fit: "w-fit",
+            },
             variant: {
-                default: "bg-neutral-300 dark:bg-neutral-600 text-neutral-950 dark:text-neutral-50 hover:bg-neutral-400 dark:hover:bg-neutral-500",
-                primary: "bg-primary text-neutral-50 hover:bg-secondary",
-                outline: "bg-transparent border border-primary text-primary hover:bg-primary hover:text-neutral-50",
-                destructive: "bg-red-500 text-neutral-50 hover:bg-red-600",
-                ghost: "bg-transparent text-neutral-950 dark:text-neutral-50 hover:bg-neutral-300 hover:dark:bg-neutral-600",
-                link: "bg-transparent text-primary underline-offset-4 hover:underline",
+                default: "bg-neutral-300 dark:bg-neutral-600 text-neutral-950 dark:text-neutral-50 hover:bg-neutral-400 dark:hover:bg-neutral-500 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-neutral-300 disabled:hover:dark:bg-neutral-600 disabled:hover:text-neutral-950 disabled:hover:dark:text-neutral-50",
+                primary: "bg-primary text-neutral-50 hover:bg-secondary disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-primary",
+                outline: "bg-transparent border border-primary text-primary hover:bg-primary hover:text-neutral-50 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:hover:text-primary",
+                destructive: "bg-red-500 text-neutral-50 hover:bg-red-600 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-red-500",
+                ghost: "bg-transparent text-neutral-950 dark:text-neutral-50 hover:bg-neutral-300 hover:dark:bg-neutral-600 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:hover:dark:bg-transparent disabled:hover:text-neutral-950 disabled:hover:dark:text-neutral-50",
+                link: "bg-transparent text-primary underline-offset-4 hover:underline disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:no-underline",
             }
         },
         defaultVariants: {
             size: "md",
             variant: "default",
+            width: "auto",
         },
     }
 )
@@ -32,12 +38,13 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & Varian
 function Button({
     variant,
     size,
+    width,
     className,
     ...props
 }: ButtonProps) {
     return (
         <button
-            className={cn(buttonVariants({ variant, size, className }))}
+            className={cn(buttonVariants({ variant, size, width, className }))}
             {...props}
         />
     )
